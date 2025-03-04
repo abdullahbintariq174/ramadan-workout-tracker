27bb5874aa92578957749692ae8b9574eef4221a
abdullahbintariq174 <140910576+abdullahbintariq174@users.noreply.github.com>
2025-03-05 02:11:03 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import RamadanWorkoutApp from './RamadanWorkoutApp';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faDumbbell, faFireAlt, faWind, faSyncAlt, faWalking, faArrowsAltH,
+  faChevronDown, faShoeprints, faArrowUp, faChevronUp, faArrowDown,
+  faHands, faMountain, faHandPointUp, faExpand, faRunning, faLevelUpAlt,
+  faCompress, faCoffee, faCloud, faHeadSide
+} from '@fortawesome/free-solid-svg-icons';
+
+// Add FontAwesome icons to library
+library.add(
+  faDumbbell, faFireAlt, faWind, faSyncAlt, faWalking, faArrowsAltH,
+  faChevronDown, faShoeprints, faArrowUp, faChevronUp, faArrowDown,
+  faHands, faMountain, faHandPointUp, faExpand, faRunning, faLevelUpAlt,
+  faCompress, faCoffee, faCloud, faHeadSide
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
