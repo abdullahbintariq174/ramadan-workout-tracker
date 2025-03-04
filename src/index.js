e866e9ed442f83e85e3498c5d4f0ea71a6f926d2
abdullahbintariq174 <140910576+abdullahbintariq174@users.noreply.github.com>
2025-03-05 02:53:28 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import RamadanWorkoutApp from './RamadanWorkoutApp';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-
-// Import all solid icons instead of individual ones
-library.add(fas);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
